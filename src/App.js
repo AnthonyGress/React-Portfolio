c26d8440cbdc7f6dca82900942a6b6efe4ac4bfa
Anthony Gress <the.techx.guy@gmail.com>
2021-08-23 18:48:37 -0400

    router working

--- src/App.js
+++ src/App.js
@@ -5,19 +5,47 @@ import Services from "./components/Services";
 import Portfolio from "./components/Portfolio";
 import Testimonials from "./components/Testimonials";
 import Carousel from "./components/Carousel";
-import Navbar from "./components/Navbar";
+import NavbarContent from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+// import { Navbar, Nav } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Carousel />
-      <Portfolio />
-      <Testimonials />
-    </div>
+    <Router>
+      <div className="App">
+        <NavbarContent />
+        {/* <Hero /> */}
+        {/* <About /> */}
+        {/* <Services /> */}
+        {/* <Carousel /> */}
+        {/* <Portfolio /> */}
+        {/* <Testimonials /> */}
+        <Switch>
+          <Route exact="true" path="/">
+            <Hero />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/testimonials">
+            <Testimonials />
+          </Route>
+          {/* <Route path="#contact">
+          <Contact />
+        </Route> */}
+          {/* <Route path="#businesscard">
+          <BusinessCard />
+        </Route> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,107 @@
 import logo from "../images/icons/android-chrome-512x512.png";
 import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer, IndexLinkContainer } from "react-router-bootstrap";
 import Container from "./Container";
 
 const NavbarContent = () => {
   return (
-    <Navbar collapseOnSelect expand="md">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            alt="penrose triangle logo"
-            src={logo}
-            width="40"
-            height="40"
-            className="d-inline-block align-center"
-          />
-          {" Anthony Gress"}
-        </Navbar.Brand>
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      variant="dark"
+      className="justify-content-center"
+    >
+      <Container style={{ marginLeft: 0, marginRight: 0 }}>
+        <LinkContainer to="/">
+          <Navbar.Brand href="#home">
+            <img
+              alt="penrose triangle logo"
+              src={logo}
+              width="40"
+              height="40"
+              className="d-inline-block"
+            />
+            {" Anthony Gress"}
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto align-items-center">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#businesscard">Business Card</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link>About</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/services">
+              <Nav.Link>Servies</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/portfolio">
+              <Nav.Link>Portfolio</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/testimonials">
+              <Nav.Link>Testimonials</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact">
+              <Nav.Link>Contact</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/businesscard">
+              <Nav.Link>Business Card</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    /* <nav className="navbar navbar-expand-md">
+        <a class="navbar-brand" href="#">
+          <img
+            src={logo}
+            width="40"
+            height="40"
+            class="d-inline-block align-center"
+            alt=""
+          />
+          {" Anthony Gress"}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link" href="#">
+              About
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Services
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Testimonials
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Contact
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Business Card
+            </a>
+          </div>
+        </div>
+      </nav> */
   );
 };
+
+function Home() {
+  return <h2>Home</h2>;
+}
 
 export default NavbarContent;
