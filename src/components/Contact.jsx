60f1d9d5fcd12b2f82fb9e1c293a724288628bc5
Anthony Gress <the.techx.guy@gmail.com>
2021-08-24 18:40:28 -0400

    remove border color on inputs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,9 +38,6 @@ const Contact = () => {
                     required
                   />
                 </FloatingLabel>
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
               </Form.Group>
               <Form.Group className="mb-3" controlId="formComment"></Form.Group>
               <FloatingLabel controlId="floatingTextarea2" label="Comments">
