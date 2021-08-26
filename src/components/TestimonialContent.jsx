38867f52276657fc46aa7cb10ee06a41c8771deb
Anthony Gress <the.techx.guy@gmail.com>
2021-08-25 20:31:33 -0400

    move lists to seperate components

---
+++ src/components/TestimonialContent.jsx
@@ -0,0 +1,47 @@
+import face1 from "../images/faces/face1.jpg";
+import face2 from "../images/faces/face2.jpg";
+import face3 from "../images/faces/face3.jpg";
+import face4 from "../images/faces/face4.jpg";
+import face5 from "../images/faces/face5.jpg";
+import face6 from "../images/faces/face6.jpg";
+
+const review1 = {
+  id: 0,
+  image: face1,
+  review:
+    "Great person all the way around. Anthony knew exactly what I was talking about and how to handle my situation. Very knowledgeable, pleasant, and had a great sense of humor. I was just very lucky to have had this great person to help me. He did an excellent job. Thanks for having such a nice man to do business with. Keep him, he is GREAT.",
+};
+const review2 = {
+  id: 1,
+  image: face2,
+  review:
+    "Anthony was very good, very knowledgeable . He helped me with the phone mess and got it fixed in no time whatsoever! This representative was the best I’ve ever dealt with. He made the awful Verizon help call better.",
+};
+const review3 = {
+  id: 2,
+  image: face3,
+  review:
+    "Outstanding customer service! Great communication. Wonderful attitude. Thank you Anthony! Very satisfied. He explained why I was having the issue versus just correcting the issue.",
+};
+const review4 = {
+  id: 3,
+  image: face4,
+  review:
+    "When they couldn’t help me at the Verizon store, they gave me the number to get further assistance. Anthony was very informative as he walked me through every step of the way.",
+};
+const review5 = {
+  id: 4,
+  image: face5,
+  review:
+    "Issue easily resolved and he was easy to communicate with. He walked me through the entire process and was patient, friendly, and overall pleasant to work with.",
+};
+const review6 = {
+  id: 5,
+  image: face6,
+  review:
+    "I don’t know how much easier he could have made it. He helped me and I would never want any other type of phone. I can usually tell when a person is sincere and really wants to help. I am an older woman and he was very patient and did not rush me.",
+};
+
+const testimonials = [review1, review2, review3, review4, review5, review6];
+
+export default testimonials;
