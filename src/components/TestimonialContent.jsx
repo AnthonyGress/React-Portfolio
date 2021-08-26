d03fcdb9ae118f808c6a89b1b1c69af47f4952dd
Anthony Gress <the.techx.guy@gmail.com>
2021-08-25 20:35:33 -0400

    reorder testimonials

--- src/components/TestimonialContent.jsx
+++ src/components/TestimonialContent.jsx
@@ -12,10 +12,10 @@ const review1 = {
     "Great person all the way around. Anthony knew exactly what I was talking about and how to handle my situation. Very knowledgeable, pleasant, and had a great sense of humor. I was just very lucky to have had this great person to help me. He did an excellent job. Thanks for having such a nice man to do business with. Keep him, he is GREAT.",
 };
 const review2 = {
-  id: 1,
-  image: face2,
+  id: 4,
+  image: face5,
   review:
-    "Anthony was very good, very knowledgeable . He helped me with the phone mess and got it fixed in no time whatsoever! This representative was the best I’ve ever dealt with. He made the awful Verizon help call better.",
+    "Issue easily resolved and he was easy to communicate with. He walked me through the entire process and was patient, friendly, and overall pleasant to work with.",
 };
 const review3 = {
   id: 2,
@@ -29,11 +29,12 @@ const review4 = {
   review:
     "When they couldn’t help me at the Verizon store, they gave me the number to get further assistance. Anthony was very informative as he walked me through every step of the way.",
 };
+
 const review5 = {
-  id: 4,
-  image: face5,
+  id: 1,
+  image: face2,
   review:
-    "Issue easily resolved and he was easy to communicate with. He walked me through the entire process and was patient, friendly, and overall pleasant to work with.",
+    "Anthony was very good, very knowledgeable . He helped me with the phone mess and got it fixed in no time whatsoever! This representative was the best I’ve ever dealt with. He made the awful Verizon help call better.",
 };
 const review6 = {
   id: 5,
