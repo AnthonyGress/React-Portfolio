1740f960cc6c2c6690a4ce92421a83a12350c70f
Anthony Gress <the.techx.guy@gmail.com>
2021-09-15 16:21:24 -0400

    added resume pdf image and updated link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,11 +3,18 @@ import Container from "./Container";
 import Row from "./Row";
 import Col from "./Col";
 import resume from "../images/resume.pdf";
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 const Resume = () => {
   return (
     <section id="resume" className="pb-5">
       <h1 className="heading">Resume</h1>
+      <div className="d-flex justify-content-center mb-5">
+        <Document file={resume} options={{ workerSrc: "/pdf.worker.js" }}>
+          <Page pageNumber={1} width={900} />
+        </Document>
+      </div>
       <Container>
         <Row>
           <Col size="col-md-8">
